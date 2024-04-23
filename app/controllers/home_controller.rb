677ae3834331a36db5b55412ea789e6ed3df38db
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-23 02:36:25 +0000

    got dynamic random page working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def home_method
-    @rand_move = ["rock", "paper", "scissors"].sample
 
     render({ :template => "page_templates/home"})
   end
--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -1,7 +1,14 @@
 class RandomController < ApplicationController
-  def home_method
+  def random_method
     @num_dice = params.fetch("number_of_dice").to_i
     @sides_dice = params.fetch("sides_per_dice").to_i
+
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides_dice)
+      @rolls.push(die)
+    end
 
     render({ :template => "page_templates/random"})
   end
