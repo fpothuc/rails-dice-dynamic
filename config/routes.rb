85f38f04a631c6edd4b913c500a3fb498b246736
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-23 03:05:48 +0000

    fixed names, app works now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "home", :action => "home_method"})
 
-  get("/:number_of_dice/:sides_per_dice", { :controller => "random", :action => "random_method"})
+  get("/dice/:number_of_dice/:sides_per_dice", { :controller => "random", :action => "random_method"})
 end
